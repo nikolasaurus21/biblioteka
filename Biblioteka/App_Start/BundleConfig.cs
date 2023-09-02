d9303244058d99d419c455941f0d4cad96d7f8cf
nikolasaurus <nikola01djikanovic@gmail.com>
2023-09-02 14:26:27 +0200

    dodat lietra stil i zapocete kartice na dashborad

--- Biblioteka/App_Start/BundleConfig.cs
+++ Biblioteka/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Biblioteka
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-litera.css",
                       "~/Content/site.css"));
         }
     }
